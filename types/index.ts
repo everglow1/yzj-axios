4e14d8c25c98fe8876ebc5cef98b72934354bb18
weidian <youzhijun@weidian.com>
2019-12-18 20:25:18 +0800

    完善代码

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,4 @@
+
 export type Method = 'get' | 'GET' | 'delete' | 'Delete' | 'head' | 'HEAD' | 'options' | 'OPTIONS' | 'post' | 'POST' | 'put' | 'PUT' | 'patch' | 'PATCH'
 
 export interface AxiosRequestConfig {
@@ -6,4 +7,17 @@ export interface AxiosRequestConfig {
 	data?: any
 	params?: any
 	headers?: any
+	responseType?: XMLHttpRequestResponseType
+}
+
+export interface AxiosResponse {
+	data: any
+	status: number
+	statusText: string
+	Headers: any
+	config: AxiosRequestConfig
+}
+
+export interface AxiosPromise extends Promise<AxiosResponse> {
+
 }
