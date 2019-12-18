11e347a07f1abb74e3c498148f888900dcd27835
weidian <youzhijun@weidian.com>
2019-12-18 14:54:10 +0800

    处理请求的da tadata

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from '../types/index'
 import xhr from './xhr'
 import {bulidURL} from '../helpers/url'
+import { transformRequest } from '../helpers/data'
 
 function axios(config: AxiosRequestConfig) {
 	processConfig(config)
@@ -9,6 +10,7 @@ function axios(config: AxiosRequestConfig) {
 
 function processConfig(config: AxiosRequestConfig): void {
 	config.url = transformUrl(config)
+	config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
@@ -16,4 +18,7 @@ function transformUrl(config: AxiosRequestConfig): string {
 	return bulidURL(url, params)
 }
 
+function transformRequestData(config: AxiosRequestConfig): any {
+	return transformRequest(config.data)
+}
 export default axios
