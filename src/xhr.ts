70c7bac2c2194ce0914e6cf7e51c5a4ef0cb5a5f
weidian <youzhijun@weidian.com>
2019-12-22 19:35:18 +0800

    处理headers

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,4 +1,5 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types'
+import { parseHeaders } from '../helpers/headers'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 
@@ -18,7 +19,8 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 					return
 				}
 	
-				const responseHeaders = request.getAllResponseHeaders()
+				const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+
 				const responseData = responseType && responseType !== 'text' ? request.response : request.responseText
 				const response: AxiosResponse = {
 					data: responseData,
