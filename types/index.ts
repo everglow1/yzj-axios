ca44907bbeae6e886fe84344f8bda2fcf14cae6d
weidian <youzhijun@weidian.com>
2019-12-23 20:17:28 +0800

    axios函数重载

--- types/index.ts
+++ types/index.ts
@@ -44,4 +44,6 @@ export interface Axios {
 
 export interface AxiosInstance extends Axios {
 	(config: AxiosRequestConfig): AxiosPromise
+
+	(url: string, config?: AxiosRequestConfig): AxiosPromise
 }
